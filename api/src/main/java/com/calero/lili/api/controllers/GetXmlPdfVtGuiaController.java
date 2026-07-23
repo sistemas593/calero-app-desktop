eee48b82fc5a6d7c51693c298e9f5ef87a0386c2
Ismael Boada <67159346+IsmaelBoada@users.noreply.github.com>
2026-07-23 10:35:04 -0500

    add: Correcion PDF

--- api/src/main/java/com/calero/lili/api/controllers/GetXmlPdfVtGuiaController.java
+++ api/src/main/java/com/calero/lili/api/controllers/GetXmlPdfVtGuiaController.java
@@ -42,7 +42,7 @@ public class GetXmlPdfVtGuiaController {
     public ResponseEntity<byte[]> descargarPdfFactura(@PathVariable("idEmpresa") Long idEmpresa,
                                                       @PathVariable("idRecibida") UUID idRecibida) {
 
-        ArchivoDto datos = vtVentasService.findPDFGuiaById(idDataService.getIdData(), idEmpresa, idRecibida, "WEB");
+        ArchivoDto datos = vtVentasService.findPDFGuiaById(idDataService.getIdData(), idEmpresa, idRecibida, "LOC");
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + datos.getNombre())
