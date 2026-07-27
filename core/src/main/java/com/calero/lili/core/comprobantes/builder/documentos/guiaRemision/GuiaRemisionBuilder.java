9da51716fb33a3c114fa84699b790d3a7f6e1a3f
Ismael Boada <67159346+IsmaelBoada@users.noreply.github.com>
2026-07-27 09:34:10 -0500

    add: Correcion Ruta en el pdf de guia de remision

--- core/src/main/java/com/calero/lili/core/comprobantes/builder/documentos/guiaRemision/GuiaRemisionBuilder.java
+++ core/src/main/java/com/calero/lili/core/comprobantes/builder/documentos/guiaRemision/GuiaRemisionBuilder.java
@@ -56,6 +56,7 @@ public class GuiaRemisionBuilder {
                 .docAduaneroUnico(guiaRemision.getDocAduaneroUnico())
                 .codEstabDestino(guiaRemision.getCodEstabDestino())
                 .detalle(builderListDetalle(guiaRemision.getDetalle()))
+                .ruta(guiaRemision.getRuta())
                 .build();
 
         builderDocSustentoInfo(guiaRemision, destinatario);
